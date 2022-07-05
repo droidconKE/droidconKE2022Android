e117206e62ceed6d1b3ee6632ff72ac88b2ade74
Evans Chepsiror <evanschepsiror@gmail.com>
2022-07-05 23:37:11 +0300

    Fix: Compose Preview

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -56,6 +56,9 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.0")
     implementation("androidx.activity:activity-compose:1.5.0")
+    implementation("androidx.customview:customview:1.2.0-alpha01")
+    implementation("androidx.customview:customview-poolingcontainer:1.0.0-rc01")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
