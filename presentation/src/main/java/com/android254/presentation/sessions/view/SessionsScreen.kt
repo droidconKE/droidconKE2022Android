cd5080a228e5015c6d8e0e1d716424ca9d92c48e
Kagiri Charles <59829833+Kagiri11@users.noreply.github.com>
2022-07-15 18:30:34 +0300

    created sessions card

--- presentation/src/main/java/com/android254/presentation/sessions/view/SessionsScreen.kt
+++ presentation/src/main/java/com/android254/presentation/sessions/view/SessionsScreen.kt
@@ -15,14 +15,30 @@
  */
 package com.android254.presentation.sessions.view
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.android254.presentation.common.components.SessionsCard
 import com.android254.presentation.common.theme.DroidconKE2022Theme
 
 @Composable
 fun SessionsScreen() {
-    Text(text = "Sessions Screen")
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        (0..10).forEach { _ ->
+            item {
+                SessionsCard()
+            }
+        }
+    }
 }
 
 @Preview
