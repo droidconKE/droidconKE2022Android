1b00713e3b2b6efea27799239c4e8562c4f4d100
Gibson Silali <gibsonsilali@gmail.com>
2022-07-18 01:33:59 +0300

    basic layout header done

--- presentation/src/main/java/com/android254/presentation/sessions/view/SessionsScreen.kt
+++ presentation/src/main/java/com/android254/presentation/sessions/view/SessionsScreen.kt
@@ -15,14 +15,61 @@
  */
 package com.android254.presentation.sessions.view
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.android254.presentation.common.components.EventDaySelectorButton
 import com.android254.presentation.common.theme.DroidconKE2022Theme
 
+data class EventDay(val date: String = "16th")
+
+val eventDays = listOf<EventDay>(
+    EventDay("16th"),
+    EventDay("17th"),
+    EventDay("18th"),
+)
+
 @Composable
-fun SessionsScreen() {
-    Text(text = "Sessions Screen")
+fun SessionsScreen(darkTheme: Boolean = isSystemInDarkTheme()) {
+    val showMySessions = remember {
+        mutableStateOf(true)
+    }
+    Column() {
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 21.dp)
+        ) {
+            Row() {
+                eventDays.forEachIndexed { index, eventDay ->
+                    EventDaySelectorButton(
+                        title = eventDay.date,
+                        subtitle = "Day ${index + 1}",
+                        onClick = { /*TODO*/ }) {
+
+                    }
+                    Spacer(Modifier.width(16.dp))
+                }
+            }
+            Switch(checked = showMySessions.value, onCheckedChange = {
+                showMySessions.value = it
+            })
+        }
+    }
 }
 
 @Preview
