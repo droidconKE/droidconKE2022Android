c11a8448c761e9c7d086079e5e027651197582ba
Gibson Silali <gibsonsilali@gmail.com>
2022-07-18 03:19:48 +0300

    WIP - [UI]session screen - session card basic layout done

--- presentation/src/main/java/com/android254/presentation/common/components/SessionsCard.kt
+++ presentation/src/main/java/com/android254/presentation/common/components/SessionsCard.kt
@@ -14,3 +14,78 @@
  * limitations under the License.
  */
 package com.android254.presentation.common.components
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Android
+import androidx.compose.material.icons.filled.StarBorder
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.android254.presentation.common.theme.Montserrat
+import com.android254.presentation.sessions.view.Event
+
+@Composable
+fun SessionsCard(session: Event) {
+    Card(
+        onClick = { /*TODO*/ },
+        modifier = Modifier.fillMaxWidth(),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White
+        ),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 36.dp
+        )
+    ) {
+        Row(
+            verticalAlignment = Alignment.Top,
+            modifier = Modifier.padding(horizontal = 17.dp, vertical = 21.dp)
+        ) {
+            Column(horizontalAlignment = Alignment.End) {
+                Text(
+                    text = session.startTime,
+                    style = TextStyle(fontSize = 18.sp, fontFamily = Montserrat)
+                )
+                Text(text = "AM")
+            }
+            Column(
+                Modifier
+                    .weight(1f)
+                    .padding(start = 24.dp, end = 0.dp, top = 0.dp, bottom = 0.dp)
+            ) {
+                Text(
+                    text = session.title,
+                    style = TextStyle(
+                        fontWeight = FontWeight.ExtraBold,
+                        fontSize = 18.sp,
+                        fontFamily = Montserrat
+                    )
+                )
+                Spacer(modifier = Modifier.height(18.dp))
+                Text(
+                    text = session.subtitle,
+                    style = TextStyle(fontSize = 16.sp, fontFamily = Montserrat)
+                )
+                Spacer(modifier = Modifier.height(12.dp))
+                Row(horizontalArrangement = Arrangement.spacedBy(14.dp)) {
+                    Text(text = "9.30AM - 10:15AM")
+                    Text(text = "|")
+                    Text(text = "ROOM 1")
+                }
+                Spacer(modifier = Modifier.height(4.dp))
+                Row(horizontalArrangement = Arrangement.spacedBy(6.dp), verticalAlignment = Alignment.CenterVertically) {
+                    Icon(Icons.Filled.Android, "", tint  = MaterialTheme.colorScheme.primary)
+                    Text(text = "Omolara Adejuwon", fontSize = 16.sp, color = MaterialTheme.colorScheme.primary)
+                }
+
+            }
+            Icon(Icons.Filled.StarBorder, "")
+        }
+    }
+}
--- presentation/src/main/java/com/android254/presentation/sessions/view/SessionsScreen.kt
+++ presentation/src/main/java/com/android254/presentation/sessions/view/SessionsScreen.kt
@@ -15,8 +15,13 @@
  */
 package com.android254.presentation.sessions.view
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
@@ -25,15 +30,28 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.android254.presentation.R
 import com.android254.presentation.common.components.EventDaySelectorButton
+import com.android254.presentation.common.components.SessionsCard
 import com.android254.presentation.common.theme.DroidconKE2022Theme
 
 data class EventDay(val date: String = "16th")
+data class Event(
+    val startTime: String,
+    val endTime: String,
+    val title: String,
+    val subtitle: String,
+    val roomLabel: String?,
+    val presenter: String?,
+    val presenterPicture: String?,
+    val isMarked: Boolean = false
+)
 
 val eventDays = listOf<EventDay>(
     EventDay("16th"),
@@ -41,18 +59,76 @@ val eventDays = listOf<EventDay>(
     EventDay("18th"),
 )
 
+val events = arrayListOf<Event>(
+    Event(
+        "09:00",
+        "09:30",
+        "Arrival",
+        "Registration & Breakfast",
+        null,
+        null,
+        null,
+        false
+    ),
+    Event(
+        "09:30",
+        "10:00",
+        "Opening",
+        "Welcome & Keynote",
+        "Room 1",
+        "Dr. John Doe",
+        null,
+        false
+    ),
+    Event(
+        "10:00",
+        "10:30",
+        "Android 254",
+        "The community and the efforts",
+        "Room 2",
+        "Greg Dawson",
+        null,
+        false
+    ),
+    Event(
+        "10:30",
+        "11:00",
+        "Break",
+        "Lunch",
+        null,
+        null,
+        null,
+        false
+    ),
+    Event(
+        "11:00",
+        "11:30",
+        "Session 1",
+        "Session 1",
+        "Room 3",
+        "Michael Kimathi",
+        null,
+        false
+    ),
+)
+
 @Composable
 fun SessionsScreen(darkTheme: Boolean = isSystemInDarkTheme()) {
     val showMySessions = remember {
         mutableStateOf(true)
     }
-    Column() {
+    Column(
+        Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.secondaryContainer)
+            .padding(horizontal = 20.dp)
+    ) {
         Row(
             horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(horizontal = 21.dp)
+                .padding(start = 0.dp, end = 0.dp, top = 24.dp, bottom = 36.dp)
         ) {
             Row() {
                 eventDays.forEachIndexed { index, eventDay ->
@@ -68,6 +144,29 @@ fun SessionsScreen(darkTheme: Boolean = isSystemInDarkTheme()) {
             Switch(checked = showMySessions.value, onCheckedChange = {
                 showMySessions.value = it
             })
+        }
+        LazyColumn(modifier = Modifier.fillMaxHeight()) {
+            itemsIndexed(events) { index, event ->
+                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.Start) {
+                    SessionsCard(session = event)
+                    if (index != events.size - 1) {
+                        Box(
+                            Modifier.padding(
+                                start = 40.dp,
+                                end = 0.dp,
+                                top = 10.dp,
+                                bottom = 10.dp
+                            )
+                        ) {
+                            Image(
+                                painter = painterResource(id = if (index % 2 == 0) R.drawable.ic_green_session_card_spacer else R.drawable.ic_orange_session_card_spacer),
+                                contentDescription = "spacer icon"
+                            )
+                        }
+                    }
+
+                }
+            }
         }
     }
 }
