52c91072ce3869d2fa8d2cd57adc698918b92127
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2022-08-14 16:03:59 +0300

    Spotless apply

--- presentation/src/test/java/com/android254/presentation/feed/view/FeedScreenTest.kt
+++ presentation/src/test/java/com/android254/presentation/feed/view/FeedScreenTest.kt
@@ -1,10 +1,24 @@
-package com.android254.presentation.feed
+/*
+ * Copyright 2022 DroidconKE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android254.presentation.feed.view
 
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import com.android254.presentation.common.theme.DroidconKE2022Theme
-import com.android254.presentation.feed.view.FeedScreen
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -37,5 +51,4 @@ class FeedScreenTest {
         composeTestRule.onNodeWithTag("feeds_lazy_column").assertExists()
         composeTestRule.onNodeWithTag("feeds_lazy_column").assertIsDisplayed()
     }
-
 }
