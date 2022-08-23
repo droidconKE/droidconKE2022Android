20819fe5b6252de118dd7da544d5ad20c44618e0
Harun Wangereka <harunwangereka@gmail.com>
2022-08-23 15:16:04 +0300

    Update AGP Versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "7.2.1" apply false
-    id("com.android.library") version "7.2.1" apply false
+    id("com.android.application") version "7.2.2" apply false
+    id("com.android.library") version "7.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.7.0" apply false
     id("com.google.devtools.ksp") version "1.7.0-1.0.6" apply true
     id("org.jlleitschuh.gradle.ktlint") version "10.2.0"
