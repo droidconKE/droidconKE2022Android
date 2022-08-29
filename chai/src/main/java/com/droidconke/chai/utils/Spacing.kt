0b6153b59729829c8cf9a2a7e86ccf88b6cdf9ae
tamzi <tamrefrank@gmail.com>
2022-08-29 23:58:11 +0300

    Added typography

--- chai/src/main/java/com/droidconke/chai/utils/Spacing.kt
+++ chai/src/main/java/com/droidconke/chai/utils/Spacing.kt
@@ -14,3 +14,17 @@
  * limitations under the License.
  */
 package com.droidconke.chai.utils
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun BreathingSpace() {
+    Spacer(modifier = Modifier.height(13.dp))
+}
+@Composable
+fun SeparatorSpace() {
+    Spacer(modifier = Modifier.height(5.dp))
+}
--- chaidemo/src/main/java/com/droidconke/chaidemo/ChaiDemoActivity.kt
+++ chaidemo/src/main/java/com/droidconke/chaidemo/ChaiDemoActivity.kt
@@ -19,7 +19,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.view.WindowCompat
-import com.droidconke.chaidemo.screens.ChaiDemo
 
 class ChaiDemoActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
