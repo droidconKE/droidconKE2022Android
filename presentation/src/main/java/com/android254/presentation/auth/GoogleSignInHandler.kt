3654de432aec6e1e9a2aab73ae5be451e4f916dc
Michael Bukachi <michaelbukachi@gmail.com>
2022-09-18 19:50:34 +0300

    refactor: Replace auth screens with auth dialog

--- presentation/src/main/java/com/android254/presentation/auth/GoogleSignInHandler.kt
+++ presentation/src/main/java/com/android254/presentation/auth/GoogleSignInHandler.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android254.presentation.auth.view
+package com.android254.presentation.auth
 
 import android.content.Context
 import android.content.Intent
