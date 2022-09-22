abc6ff669b7e37516309351c35b3bf84affe0949
BKinya <gitongakinya1@gmail.com>
2022-09-22 19:05:10 +0300

    add SpeakerViewModel

---
+++ presentation/src/main/java/com/android254/presentation/models/Speaker.kt
@@ -0,0 +1,10 @@
+package com.android254.presentation.models
+
+/**
+ * Sample speaker data class
+ */
+data class Speaker(
+    val imageUrl: String = "",
+    val name: String = "Name",
+    val bio: String = "bio"
+)
