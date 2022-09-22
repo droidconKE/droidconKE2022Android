c47faeaa90fab004aba4e6cb160131f7e3b4dbda
BKinya <gitongakinya1@gmail.com>
2022-09-22 19:27:57 +0300

    add Speakers screen

---
+++ presentation/src/main/java/com/android254/presentation/speakers/view/SpeakersScreen.kt
@@ -0,0 +1,77 @@
+package com.android254.presentation.speakers.view
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SmallTopAppBar
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.android254.presentation.R
+import com.android254.presentation.common.theme.DroidconKE2022Theme
+import com.android254.presentation.speakers.SpeakerViewModel
+
+@Composable
+fun SpeakersScreen(speakersViewModel: SpeakerViewModel = hiltViewModel()) {
+    val speakers = speakersViewModel.getSpeakers()
+    Scaffold(
+        topBar = {
+            SmallTopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.speakers_label),
+                        fontSize = 24.sp,
+                        fontFamily = FontFamily(Font(R.font.montserrat_regular)),
+                        color = colorResource(id = R.color.dark)
+                    )
+                },
+                navigationIcon = {
+                    IconButton(
+                        onClick = { },
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_back_arrow),
+                            contentDescription = stringResource(R.string.back_arrow_icon_description),
+                            tint = colorResource(id = R.color.dark)
+                        )
+                    }
+                },
+                colors = TopAppBarDefaults.largeTopAppBarColors(
+                    containerColor = Color.Transparent,
+                    titleContentColor = colorResource(id = R.color.dark),
+                    navigationIconContentColor = Color.White
+                )
+            )
+        }
+    ) { paddingValues ->
+        LazyVerticalGrid(
+            columns = GridCells.Adaptive(160.dp),
+            contentPadding = PaddingValues(start = 16.dp, end = 16.dp, top = 80.dp, bottom = 16.dp)
+        ) {
+            items(speakers) { speaker ->
+                SpeakerComponent(speaker = speaker)
+            }
+        }
+    }
+}
+@Preview
+@Composable
+fun SpeakersScreenPreview() {
+    DroidconKE2022Theme {
+        SpeakersScreen()
+    }
+}
