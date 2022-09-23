cbe8b130083fe8da7bdf4469cbb0f776f8425591
Gibson Silali <gibsonsilali@gmail.com>
2022-09-23 14:52:08 +0300

    ran ./codeAnalysis.sh

--- data/src/test/java/com/android254/data/network/SessionApiTest.kt
+++ data/src/test/java/com/android254/data/network/SessionApiTest.kt
@@ -1,17 +1,28 @@
+/*
+ * Copyright 2022 DroidconKE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.android254.data.network
 
-import com.android254.data.network.apis.AuthApi
 import com.android254.data.network.apis.SessionApi
 import com.android254.data.network.models.responses.GenericPaginatedResponse
 import com.android254.data.network.models.responses.PaginationMetaData
 import com.android254.data.network.models.responses.ResponseMetaData
 import com.android254.data.network.models.responses.SessionApiModel
 import com.android254.data.network.util.HttpClientFactory
-import com.android254.data.network.util.ServerError
-import com.android254.data.preferences.DefaultTokenProvider
 import io.ktor.client.engine.mock.*
 import io.ktor.http.*
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
@@ -38,9 +49,8 @@ class SessionApiTest {
                 )
             )
 
-
         val mockHttpEngine = MockEngine {
-            if (it.method == HttpMethod.Get && it.url.toString() == "https://api.droidcon.test/v1/events/droidconke2019-444/sessions?per_page=20") {
+            if (it.method == HttpMethod.Get && it.url.toString() == "${Constants.BASE_URL}/events/droidconke2019-444/sessions?per_page=20") {
                 respond(
                     content = Json.encodeToString(expectedResponse),
                     headers = headersOf(HttpHeaders.ContentType, "application/json")
