81dd7c4ef10b44e1b2fcc3e4a92fe86321924d82
Theophilus Kibet <kibettheophilus@gmail.com>
2022-09-27 12:50:28 +0300

    finish up setting CrashlyticsTree.kt

--- app/src/main/java/com/android254/droidconKE2022/crashlytics/CrashlyticsTree.kt
+++ app/src/main/java/com/android254/droidconKE2022/crashlytics/CrashlyticsTree.kt
@@ -1,9 +1,38 @@
 package com.android254.droidconKE2022.crashlytics
 
+import android.util.Log
+import com.android254.droidconKE2022.BuildConfig
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import timber.log.Timber
 
 class CrashlyticsTree : Timber.Tree() {
+    private val crashlytics = FirebaseCrashlytics.getInstance()
     override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
-        TODO("Not yet implemented")
+        if (priority == Log.VERBOSE || priority == Log.DEBUG || priority == Log.INFO) {
+            return
+        }
+
+        if (BuildConfig.DEBUG) {
+            crashlytics.setCrashlyticsCollectionEnabled(false)
+            return
+        }
+
+        crashlytics.setCustomKey(CRASHLYTICS_KEY_PRIORITY, priority)
+        if (tag != null) {
+            crashlytics.setCustomKey(CRASHLYTICS_KEY_TAG, tag)
+        }
+        crashlytics.setCustomKey(CRASHLYTICS_KEY_MESSAGE, message)
+
+        if (t == null) {
+            crashlytics.recordException(Exception(message))
+        } else {
+            crashlytics.recordException(t)
+        }
+    }
+
+    companion object {
+        private const val CRASHLYTICS_KEY_PRIORITY = "priority"
+        private const val CRASHLYTICS_KEY_TAG = "tag"
+        private const val CRASHLYTICS_KEY_MESSAGE = "message"
     }
 }
